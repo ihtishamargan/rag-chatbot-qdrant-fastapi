46fca0acc1b1ca7028583bec5b87d42fc62a5fcc
Ihtisham Ahmad <ihtishamargan@gmail.com>
2024-07-16 13:26:54 +0200

    feat: add text splitter

--- src/ingestion/ingest.py
+++ src/ingestion/ingest.py
@@ -8,14 +8,16 @@ documents, sheets, and PDFs.
 
 import logging
 import sys
-from collections.abc import Generator
+from collections.abc import Generator, Iterable
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import (
     GoogleDriveLoader,
     UnstructuredFileIOLoader,
 )
 
-from src.config import GOOGLE_ACCOUNT_FILE
-
+from src.config import GOOGLE_ACCOUNT_FILE, DEFAULT_CHUNK_SIZE
+from unstructured.documents.base import Document
 
 logger = logging.getLogger(__name__)
 
@@ -54,3 +56,27 @@ def load_gdrive_documents(folder_id: str, ingestion_id: str) -> Generator[dict, 
     except ValueError as e:
         logger.error("Failed to load documents: %s", e)
         sys.exit(1)
+
+
+def split_documents(documents: Iterable[dict], chunk_size: int = DEFAULT_CHUNK_SIZE) -> Generator[Document, None, None]:
+    """Split documents into smaller chunks based on the specified chunk size.
+
+    Parameters:
+        documents (List[Document]): A list of documents to be split.
+        chunk_size (int, optional): The size of each chunk. Defaults to DEFAULT_CHUNK_SIZE.
+
+    Yields:
+        Document: Yields document chunks one by one.
+
+    Raises:
+        SystemExit: If document splitting fails.
+    """
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=0,
+        separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+    )
+    doc_splits = text_splitter.split_documents(documents)  # type: ignore[arg-type]
+    for idx, split in enumerate(doc_splits):
+        split.metadata["chunk"] = idx
+        yield split
